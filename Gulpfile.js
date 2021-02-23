510f20c2d68797b02aa401a77a390bd97bddea4d
Jon Savage <jon@studioaceofspades.com>
2021-02-23 16:33:11 +0000

    Gulp fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,3 @@
-
-const  webfontsGenerator  =  require('webfonts-generator');
 const  fs                 =  require('fs');
 const  gulp               =  require('gulp');
 const  sass               =  require('gulp-sass');
