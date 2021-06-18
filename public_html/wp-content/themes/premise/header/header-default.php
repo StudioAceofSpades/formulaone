d31f43fd603f6c4f15a0bbc547a7fe2b8681a0e4
antphan98 <antphan98@gmail.com>
2021-06-18 14:38:02 +0000

    set up header and added some styles

--- public_html/wp-content/themes/premise/header/header-default.php
+++ public_html/wp-content/themes/premise/header/header-default.php
@@ -23,12 +23,55 @@ E-Mail: jon@studioaceofspade.com
 <body id="top-of-page">
 
 <header class="header cf">
-    <h1 class="brand">
-        <a href="<?php bloginfo('url'); ?>">
-            <img src="<?php bloginfo('template_directory'); ?>/img/demo.png">
-        </a>
-    </h1>
-	<?php include(locate_template('parts/navigation.php')); ?>
+    <nav class="secondary">
+            <div class="row">
+            <a href="<?php bloginfo('url'); ?>" class="brand">
+                <img src="<?php bloginfo('template_directory'); ?>/img/Group 33.png" alt="<?php bloginfo('name'); ?>" class="formulaicon">
+            </a>
+                <div class="col d-flex justify-content-end">
+                    <ul>
+                        <li><a class="active" href="#home">Resources</a></li>
+                        <li><a href="#news">Dealer Login</a></li>
+                        <li><a href="#contact">About</a></li>
+                        <li><a href="#about">Careers</a></li>
+                    </ul>
+                    <ul>
+                    <li><a class="phone" href="tel:888-622-0828">888-622-0828</a></li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-facebook">Facebook</i>
+                            </a>
+                        </li><li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-youtube">YouTube</i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-linkedin">LinkedIn</i>
+                            </a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+    </nav>
+<div class="main">
+    <div class="row justify-content-between">
+  
+        <div class="col d-flex flex-grow-1 justify-content-end">
+            <nav class="primary">
+                    <ul>
+                        <li><a class="active" href="#home">Cargo</a></li>
+                        <li><a href="#news">Racing</a></li>
+                        <li><a href="#contact">Commercial</a></li>
+                        <li><a href="#about">Recreational</a></li>
+                        <li><a href="#about">Contact</a></li>
+                        <li><a class="button" href="#about">Build Yours</a></li>
+                    </ul>
+            </nav>
+        </div>
+    </div>
+</div>
 </header>
 
 <main id="main">
