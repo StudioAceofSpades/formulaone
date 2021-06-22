8a3b5a0739aed18fe8bdb88ad91cd9d350cd52ed
antphan98 <antphan98@gmail.com>
2021-06-22 20:57:58 +0000

    fixed some styling and added acf fields

--- public_html/wp-content/themes/premise/header/header-default.php
+++ public_html/wp-content/themes/premise/header/header-default.php
@@ -21,65 +21,68 @@ E-Mail: jon@studioaceofspade.com
 </head>
 
 <body id="top-of-page">
-
-<header class="header cf">
-    <nav class="secondary">
+    <header class="header cf">
+        <nav class="secondary">
             <div class="row">
-            <a href="<?php bloginfo('url'); ?>" class="brand">
-                <img src="<?php bloginfo('template_directory'); ?>/img/Group 33.png" alt="<?php bloginfo('name'); ?>" class="formulaicon">
-            </a>
-                <div class="col d-flex justify-content-end">
-                <?php if(have_rows('top_navigation','options')): ?>
-                    <ul>
-                    <?php 
-                    while(have_rows('top_navigation','options')) : 
-                        the_row(); 
-                        $link = get_sub_field('menu_link'); ?>
-                        <li>
+                <a href="<?php bloginfo('url'); ?>" class="brand">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/formula-trailer-logo.png" alt="<?php bloginfo('name'); ?>" class="formulaicon">
+                </a>
+                    <div class="col d-flex justify-content-end">
+                        <div class="top-menu-links">
+                            <?php if(have_rows('top_navigation','options')): ?>
+                                <ul>
+                                <?php 
+                                while(have_rows('top_navigation','options')) : 
+                                    the_row(); 
+                                    $link = get_sub_field('menu_link'); ?>
+                                    <li>
                                         <?php saos_output_link($link); ?>
-                        </li>
-                        <?php endwhile; ?>
-
-                    </ul>
-                    <?php endif; ?>
-
-                    <ul>
-                    <li><a class="phone" href="tel:888-622-0828">888-622-0828</a></li>
-                        <li>
-                            <a href="#" target="_blank">
-                                <i class="fab fa-facebook">Facebook</i>
-                            </a>
-                        </li><li>
-                            <a href="#" target="_blank">
-                                <i class="fab fa-youtube">YouTube</i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" target="_blank">
-                                <i class="fab fa-linkedin">LinkedIn</i>
-                            </a>
-                        </li>
-                    </ul>
+                                    </li>
+                            <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
+                        </div>
+                        <div class="top-menu-social">
+                            <ul>
+                            <li><a class="phone" href="tel:888-622-0828">888-622-0828</a></li>
+                                <li>
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-facebook"></i>
+                                    </a>
+                                </li><li>
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-youtube"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank">
+                                    <i class="fab fa-linkedin"></i>                            
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-    </nav>
-<div class="main">
-    <div class="row justify-content-between">
-  
-        <div class="col d-flex flex-grow-1 justify-content-end">
-            <nav class="primary">
-                    <ul>
-                        <li><a class="active" href="#home">Cargo</a></li>
-                        <li><a href="#news">Racing</a></li>
-                        <li><a href="#contact">Commercial</a></li>
-                        <li><a href="#about">Recreational</a></li>
-                        <li><a href="#about">Contact</a></li>
-                        <li><a class="button" href="#about">Build Yours</a></li>
-                    </ul>
-            </nav>
+            </div>
+        </nav>
+    <div class="main">
+        <div class="row justify-content-between">
+            <div class="col d-flex flex-grow-1 justify-content-end">
+                <nav class="primary">
+                        <ul>
+                            <div class="primary-links">
+                                <li><a class="active" href="#home">Cargo</a></li>
+                                <li><a href="#news">Racing</a></li>
+                                <li><a href="#contact">Commercial</a></li>
+                                <li><a href="#about">Recreational</a></li>
+                                <li><a href="#about">Contact</a></li>
+                            <li><a class="button" href="#about">Build Yours</a></li>
+                            </div>
+
+                        </ul>
+                </nav>
+            </div>
         </div>
     </div>
-</div>
-</header>
+    </header>
 
 <main id="main">
