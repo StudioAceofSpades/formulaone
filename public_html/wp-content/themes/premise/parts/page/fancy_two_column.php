e6aa81e4774837db2f7a52093e6d478bb69969b3
SrishtiAvalakki <spram001@odu.edu>
2021-07-07 20:44:11 +0000

    fancy 2 col and code cleanup

---
+++ public_html/wp-content/themes/premise/parts/page/fancy_two_column.php
@@ -0,0 +1,40 @@
+<div class="fancy-two-column">
+    <?php if (have_rows('fancy_two_col')): ?>
+        <?php while(have_rows('fancy_two_col') ): the_row(); ?>
+        <div class="header bg-orange">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col text-center">
+                        <?php if(get_sub_field('header')): ?>
+                            <?php $header = get_sub_field('header'); ?>
+                            <h1 class="header"><?php echo $header ?></h1>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content bg-gray">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6 secondary">
+                        <div class="header_card">
+                            <?php if(get_sub_field('block_left')): ?>
+                                <?php $block_left = get_sub_field('block_left'); ?>
+                                    <?php echo $block_left ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6 secondary">
+                        <div class="header_card">
+                            <?php if(get_sub_field('block_right')): ?>
+                                <?php $block_right = get_sub_field('block_right'); ?>
+                                    <?php echo $block_right ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</div>
