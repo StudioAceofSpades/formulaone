964f586a4eb6d96aad38a36d4863d4fbb1f2dde5
Jon Savage <jon@studioaceofspades.com>
2021-07-20 16:54:24 +0000

    added cpt/tax info

--- public_html/wp-content/themes/premise/functions.php
+++ public_html/wp-content/themes/premise/functions.php
@@ -145,4 +145,79 @@ function saos_output_link($link, $classes="") {
 	}
 }
 
+function cptui_register_my_cpts_trailer() {
+
+	/**
+	 * Post Type: Trailers.
+	 */
+
+	$labels = [
+		"name" => __( "Trailers", "custom-post-type-ui" ),
+		"singular_name" => __( "Trailer", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Trailers", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "trailer", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "trailer", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_trailer' );
+
+function cptui_register_my_taxes_lifestyle() {
+
+	/**
+	 * Taxonomy: Lifestyles.
+	 */
+
+	$labels = [
+		"name" => __( "Lifestyles", "custom-post-type-ui" ),
+		"singular_name" => __( "Lifestyle", "custom-post-type-ui" ),
+	];
+
+	
+	$args = [
+		"label" => __( "Lifestyles", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => false,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'lifestyle', 'with_front' => true, ],
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "lifestyle",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		"show_in_graphql" => false,
+	];
+	register_taxonomy( "lifestyle", [ "trailer" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_lifestyle' );
+
+
 ?>
