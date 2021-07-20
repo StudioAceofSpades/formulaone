02ae3c2151e82680ca9dda9f5b9aa17effdb6b69
saosdev <jon@studioaceofspades.com>
2021-07-20 17:11:22 +0000

    hero was broken

--- public_html/wp-content/themes/premise/parts/hero.php
+++ public_html/wp-content/themes/premise/parts/hero.php
@@ -3,7 +3,7 @@
         <?php include(locate_template('parts/hero/hero-home.php')); ?>
 	<?php elseif(is_page_template('single-standardsubpage.php')): ?>
         <?php include(locate_template('parts/hero/hero-standardsubpage.php')); ?>
-        <?php elseif(is_page_template('single-contact.php')): ?>
+    <?php elseif(is_page_template('single-contact.php')): ?>
         <?php include(locate_template('parts/hero/hero-contact.php')); ?>
     <?php else: ?>
         <?php include(locate_template('parts/hero/hero-default.php')); ?>
