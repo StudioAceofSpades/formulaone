4bdab4c0fbc1325e9145b6e76f0a145ac4f2286f
tedlogic <tedlogic@gmail.com>
2021-09-09 19:06:31 +0000

    Fixed all lifestyles page

--- public_html/wp-content/themes/premise/pages/lifestyle.php
+++ public_html/wp-content/themes/premise/pages/lifestyle.php
@@ -4,42 +4,54 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 get_header(); ?>
 
-<?php include(locate_template('parts/hero.php')); ?>
+<div class="all-lifestyle">
 
-    <div class="lifestyle">
+    <section class="about bg-white">
+        <div class="container">
 
-        <section class="about bg-white">
-            <div class="container">
+            <?php if($header = get_field('lifestyle_header')): ?>
+            <div class="row">
+                <div class="col text-center">
+                    <h1><?php echo $header; ?></h1>
+                </div>
+            </div>
+            <?php endif; ?>
 
-                <?php if($header = get_field('lifestyle_header')): ?>
-                <div class="row">
-                    <div class="col text-center">
-                        <p class="title"><?php echo $header; ?></p>
+            <?php if(have_rows('lifestyles')): ?>
+                <div class="lifestyle">
+                    <div class="row">
+
+                    <?php while(have_rows('lifestyles')) : the_row(); ?>
+                        <div class="col-md-6">
+                            <a href="<?php the_sub_field('link') ?>" style="background-image: url(<?php the_sub_field('image'); ?>);">
+                                <span><?php the_sub_field('title'); ?></span>
+                            </a>
+                        </div>
+                    <?php endwhile; ?>
+
+
+<!--
+                        <?php while(have_rows('lifestyles')) : the_row(); ?>
+                            <div class="col-lg-6 col-md-6 col-sm-12">
+                                <?php if($icon = get_sub_field('image')): ?>
+                                    <div class="image" style="background-image: url(<?php echo $icon['url']; ?>);">
+                                        <?php if($title = get_sub_field('title')): ?>
+                                            <h3><?php echo $title; ?></h3>
+                                        <?php endif; ?>
+                                        <?php if($link = get_sub_field('link')): ?>
+                                            <a href="<?php echo $link['url']; ?>" class="panel-link"></a>
+                                        <?php endif; ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        <?php endwhile; ?>
+
+                                        -->
+
                     </div>
                 </div>
-                <?php endif; ?>
-
-                <?php if(have_rows('lifestyles')): ?>
-                    <div class="lifestyle">
-                        <div class="row">
-                            <?php while(have_rows('lifestyles')) : the_row(); ?>
-                                <div class="col-lg-6 col-md-6 col-sm-12">
-                                    <?php if($icon = get_sub_field('image')): ?>
-                                        <div class="image" style="background-image: url(<?php echo $icon['url']; ?>);">
-                                            <?php if($title = get_sub_field('title')): ?>
-                                                <h3><?php echo $title; ?></h3>
-                                            <?php endif; ?>
-                                            <?php if($link = get_sub_field('link')): ?>
-                                                <a href="<?php echo $link['url']; ?>" class="panel-link"></a>
-                                            <?php endif; ?>
-                                        </div>
-                                    <?php endif; ?>
-                                </div>
-                            <?php endwhile; ?>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            <div>
-        <section>
-    </div>
-    <?php get_footer(); ?>
+            <?php endif; ?>
+        <div>
+    <section>
+</div>
+<?php get_footer(); ?>
