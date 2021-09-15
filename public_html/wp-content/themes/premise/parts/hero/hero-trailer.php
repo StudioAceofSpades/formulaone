689466381c2ffe5aa53fadac3100b3f9dc655aeb
tedlogic <tedlogic@gmail.com>
2021-09-15 19:01:15 +0000

    added video hero to single trailer page

--- public_html/wp-content/themes/premise/parts/hero/hero-trailer.php
+++ public_html/wp-content/themes/premise/parts/hero/hero-trailer.php
@@ -1,6 +1,26 @@
-<?php $bg = get_field('background_image'); ?>
+<?php
+if(is_tax()) {
+    $term = get_queried_object();
+} else {
+    $term = false;
+}
 
-<div class="hero-wrapper hero-trailer" style="background-image: url(<?php echo ($bg['url'])?>)">
+$background = create_bg($term);
+if(isset($background['image'])) {
+    $bg_image_output = 'style="background-image: url(';
+    $bg_image_output .= $background['image'];
+    $bg_image_output .=');"';
+} else {
+    $video_output = '<video playsinline autoplay muted loop poster="' . $background['cover'] . '">';
+    $video_output .= '<source src="' . $background['webm'] . '" type="video/webm">';
+    $video_output .= '<source src="' . $background['mp4'] . '" type="video/mp4">';
+    $video_output .= '</video>';
+}
+?>
+
+<div class="hero-wrapper hero-trailer" <?php echo $bg_image_output; ?>>
+    <?php echo $video_output; ?>
+
     <div class="title-wrapper">
         <?php
         if (get_field('page_title')):
