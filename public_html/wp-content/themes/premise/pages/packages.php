765604269185fa0fe0b06c55a4afda450af542db
saosdev <jon@studioaceofspades.com>
2021-12-22 23:13:16 +0000

    final

--- public_html/wp-content/themes/premise/pages/packages.php
+++ public_html/wp-content/themes/premise/pages/packages.php
@@ -10,49 +10,49 @@ get_header(); ?>
 
     <div class="container">
         
-        <?php if($trailers = get_field('configurations')): ?>
         <div class="packages">
+                
             <?php
             global $post;
-            foreach($trailers as $post): setup_postdata($post); ?>
-            <div class="config">
-                
-                <h3><?php the_title(); ?></h3>
+            $args = array(
+                'post_type'         => 'package',
+                'posts_per_page'    => -1,
+                'orderby'           => 'menu_order',
+            );
+            $posts = get_posts($args);
 
+            if($posts): 
+                $counter = 0;
+                ?>
+                <div class="row">
                 <?php 
-                if($packages = get_field('packages')): 
-                    $counter = 0;
+                foreach($posts as $post) : setup_postdata($post); 
+                    $counter++;
                     ?>
-                    <div class="row">
-                    <?php 
-                    foreach($packages as $package): 
-                        $pid = $package->ID; 
-                        $counter++;
-                        ?>
-                        <div class="col-md-4">
-                            <div class="package">
-                                <h4><?php echo get_the_title($pid); ?></h4>
-                                <?php if(have_rows('options', $pid)): ?>
-                                <ul class="options">
-                                    <?php while(have_rows('options', $pid)) : the_row(); ?>
-                                        <li><?php the_sub_field('option_name'); ?></li>
-                                    <?php endwhile; ?>
-                                </ul>
-                                <?php endif; ?>
-                            </div>
+                    <div class="col-md-4">
+                        <div class="package">
+                            <h4><?php the_title(); ?></h4>
+                            <?php if(have_rows('options')): ?>
+                            <ul class="options">
+                                <?php while(have_rows('options')) : the_row(); ?>
+                                    <li><?php the_sub_field('option_name'); ?></li>
+                                <?php endwhile; ?>
+                            </ul>
+                            <?php endif; ?>
                         </div>
-                        <?php if($counter % 3 == 0): ?>
-                        </div>
-                        <div class="row">
-                        <?php endif; ?>
-                        
-                    <?php endforeach; ?>
                     </div>
-                <?php endif; ?>
-            </div>
-            <?php endforeach; wp_reset_postdata(); ?>
+                    <?php if($counter % 3 == 0): ?>
+                    </div>
+                    <div class="row">
+                    <?php endif; ?>
+                    
+                <?php 
+                endforeach; 
+                wp_reset_postdata();
+                ?>
+                </div>
+            <?php endif; ?>
         </div>
-        <?php endif; ?>
 
     </div>
 </div>
