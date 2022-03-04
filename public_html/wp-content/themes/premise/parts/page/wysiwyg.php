2127979254dc8f3e9e55081cd205956d69dc35e9
saosdev <jon@studioaceofspades.com>
2022-03-04 21:15:37 +0000

    added a wide layout for wysiwyg content block

--- public_html/wp-content/themes/premise/parts/page/wysiwyg.php
+++ public_html/wp-content/themes/premise/parts/page/wysiwyg.php
@@ -1,4 +1,8 @@
+<?php if(get_sub_field('sizing') == 'wide'): ?>
+<div class="wysiwyg wide">
+<?php else: ?>
 <div class="wysiwyg">
+<?php endif; ?>
     <div class="container">
         <?php the_sub_field('wysiwyg'); ?>
     </div>
